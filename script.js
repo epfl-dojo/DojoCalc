ac4f13b4407242bdc02308a090d151adbb2e5d32
Giovanni Cangiani <giovanni.cangiani@epfl.ch>
2020-08-28 10:22:52 +0000

    POC with css grid (js not yet working)

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 console.log("Hello Dojo");
 
-let allInput = document.querySelectorAll("input[type=button]");
+let allInput = document.querySelectorAll("button");
 for(i = 0; i < allInput.length; i++){
-  console.log(allInput[i].value);
+  console.log(allInput[i].innerHTML);
   allInput[i].addEventListener("click", test, false);
 } 
 
 function test(){
   //alert(this.value);
   txt = document.getElementById('zoneTxt');
-  txt.value = txt.value + ' ' + this.value;
+  txt.value = txt.value + '' + this.value;
 }
