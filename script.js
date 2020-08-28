3a6e800a256eec6c33dd6f79e4c9a3f730ffeb83
Nicolas Borboën <ponsfrilus@gmail.com>
2020-08-28 10:56:38 +0000

    Basic function to render display + cosmetics

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-console.log("Hello Dojo");
+console.log("🧠🧠🧠 thinking hard 🧠🧠🧠");
 
-let allInput = document.querySelectorAll("button");
+let allInput = document.getElementsByTagName('button');
 for(i = 0; i < allInput.length; i++){
-  console.log(allInput[i].innerHTML);
+  // console.log(allInput[i].innerHTML);
   allInput[i].addEventListener("click", test, false);
 } 
 
 function test(){
-  //alert(this.value);
-  txt = document.getElementById('zoneTxt');
-  txt.value = txt.value + '' + this.value;
+  // alert(this.innerHTML);
+  txt = document.getElementById('calc-display1');
+  if (this.className.includes('digit')) {
+   txt.innerHTML = txt.innerHTML + this.innerHTML;
+  }
+  if (this.className.includes('operator')) {
+    if (this.className.includes('button-ce')) {
+      txt.innerHTML = '';
+    } else {
+      // Le "+" ne fonctionne pas
+      txt.innerHTML = txt.innerHTML + this.innerHTML;
+    }
+  }
+  if (this.className.includes('button-submit')) {
+    txt.innerHTML = "🧠THINK🧠";
+  }
 }
