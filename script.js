a183f080dc47d0e361ad7171b43669536d9ceedd
Nicolas Reymond <n.reymond.12@gmail.com>
2020-09-04 10:01:32 +0000

    [F.T] Some new featurs

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 console.log("🧠🧠🧠 thinking hard 🧠🧠🧠");
 
-let allInput = document.getElementsByTagName('button');
-for(i = 0; i < allInput.length; i++){
+let allButtons = document.getElementsByTagName('button');
+
+let operator = new Array('+', '-', '*', '/', '=');
+
+for (i = 0; i < allButtons.length; i++) {
   // console.log(allInput[i].innerHTML);
-  allInput[i].addEventListener("click", test, false);
+  allButtons[i].addEventListener("click", test, false);
 } 
 
 function test(){
   // alert(this.innerHTML);
-  txt = document.getElementById('calc-display1');
+  txt = document.getElementById('calc-display');
   if (this.className.includes('digit')) {
-   txt.innerHTML = txt.innerHTML + this.innerHTML;
+    txt.innerHTML = txt.innerHTML + this.innerHTML;
   }
   if (this.className.includes('operator')) {
     if (this.className.includes('button-ce')) {
       txt.innerHTML = '';
     } else {
-      // Le "+" ne fonctionne pas
       txt.innerHTML = txt.innerHTML + this.innerHTML;
     }
   }
   if (this.className.includes('button-submit')) {
-    txt.innerHTML = "🧠THINK🧠";
+    console.log(escapeRegExp(txt.innerHTML));
+    txt.innerHTML = eval(escapeRegExp(txt.innerHTML));  
   }
+}
+
+function escapeRegExp(string) {
+  return string.replace(/[^\d+-\/*]*/g, '');
 }
